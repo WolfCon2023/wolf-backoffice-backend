453cc432e63e6a1703b305fa7e8680d8a405ab5a
WolfCon2023 <ccarter@wolfconsultingnc.com>
2025-02-13 19:19:47 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,16 @@
-const express = require("express");
-const { loginUser, registerUser } = require("../controllers/authController");
+const jwt = require("jsonwebtoken");
 
-const router = express.Router();
+const verifyToken = (req, res, next) => {
+    const token = req.header("Authorization");
+    if (!token) return res.status(403).json({ message: "Access Denied" });
+
+    try {
+        const verified = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: "Invalid Token" });
+    }
+};
+
+module.exports = verifyToken;
