a611cff8402ec386dc3fcc2a17f001209fc0e345
WolfCon2023 <ccarter@wolfconsultingnc.com>
2025-02-13 19:27:00 -0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,3 +2,11 @@ const express = require("express");
 const { loginUser, registerUser } = require("../controllers/authController");
 
 const router = express.Router();
+
+// Register New User
+router.post("/register", registerUser);
+
+// Login User
+router.post("/login", loginUser);
+
+module.exports = router;
