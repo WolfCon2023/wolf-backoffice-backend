4aa0d9f5e2d16bc929a1be661eaae7a3ebdc9073
WolfCon2023 <ccarter@wolfconsultingnc.com>
2025-02-13 19:10:38 -0500

    Update auth.js
    - Uses the new `authController.js` functions.
    - Cleaner and modular.

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,3 @@ const express = require("express");
 const { loginUser, registerUser } = require("../controllers/authController");
 
 const router = express.Router();
-
-// Register New User
-router.post("/register", registerUser);
-
-// Login User
-router.post("/login", loginUser);
-
-module.exports = router;
-
